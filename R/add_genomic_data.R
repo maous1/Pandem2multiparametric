1861fb5b7a042e0450aa646ba02097b17854d52c
MBonjean <67752577+maous1@users.noreply.github.com>
2022-04-29 14:58:02 +0200

    exchange

---
+++ R/add_genomic_data.R
@@ -0,0 +1,68 @@
+#' Title
+#'
+#' @param metadata
+#' @param genomic_data
+#' @param col_merge
+#' @param count
+#'
+#' @return
+#' @export
+#'
+#' @examples
+add_genomic_data <- function(metadata, genomic_data, col_merge, count) {
+  if (!any(names(metadata) %in% col_merge)) {
+    if (!any(names(genomic_data) %in% col_merge)) {
+      stop("wrong col_merge in genomic_data and metadata")
+    }
+    stop("wrong col_merge in  metadata")
+  }
+  if (!any(names(genomic_data) %in% col_merge)) {
+    stop("wrong col_merge in genomic_data")
+  }
+
+  if (!any(names(metadata) %in% count)) {
+    stop("wrong count in metadata")
+  }
+
+
+  names(metadata)[names(metadata) %in% col_merge] <- "variant_metadata"
+  names(genomic_data)[names(genomic_data) %in% col_merge] <- "variant"
+
+  metadata <- expandRows(metadata, count = count, drop = T)
+
+  # Create table with genomic data associated to sample metadata
+  genomic_data_with_metadata <- tibble()
+
+  sample_genomic <- function(tmp_variant,tmp_sample_metadata)
+  {
+    sample_genomic_data <- tmp_variant[sample(
+      x = nrow(tmp_variant),
+      size = nrow(tmp_sample_metadata),
+      replace = TRUE
+    ), ]
+    tmp <- cbind(tmp_sample_metadata,sample_genomic_data)
+  }
+
+  genomic_data <- genomic_data %>% split(.$variant)
+  metadata_split = metadata %>% split(.$variant_metadata)
+  common_variant <- intersect(names(genomic_data), names(metadata_split))
+
+  genomic_data <- genomic_data[is.element(names(genomic_data), common_variant)]
+  metadata_NSQ <- do.call(rbind, metadata_split[!is.element(names(metadata_split), common_variant)])
+
+  metadata_SQ <- metadata_split[is.element(names(metadata_split), common_variant)]
+
+  genomic_data_with_metadata <- map2_df(.x = genomic_data, .y = metadata_SQ, .f = function(.x, .y) sample_genomic(tmp_variant = .x, tmp_sample_metadata = .y))
+
+
+
+  genomic_data_with_metadata <- union_all(genomic_data_with_metadata, metadata_NSQ)
+  # Remove original country column, collection date and ecdc variant (redundant)
+  genomic_data_with_metadata <- genomic_data_with_metadata %>%
+    select(-scorpio_call,-substitutions,-deletions,-missing,-insertions,-country, -variant, -collection_date,-lineage,-pangolin_version,-clade,-biosample,-SRA_accession,-accession)%>%
+    group_by_all() %>%
+    summarise(nb = n(), .groups = "drop")
+
+  names(genomic_data_with_metadata)[names(genomic_data_with_metadata) %in% "variant_metadata"] <- col_merge
+  return(genomic_data_with_metadata)
+}
---
+++ README.md
@@ -0,0 +1,35 @@
+# Pandem2simulator
+Here is the command to install the package
+
+```
+install.packages("devtools")
+devtools::install_github("maous1/Pandem2simulator")
+```
+
+I propose a script to test the package
+
+```
+library(Pandem2simulator)
+
+# Loading the data
+data("variants_aggregated")
+data("case_aggregated")
+# Formated the files
+
+case_aggregated_format <- format_case(case_aggregated,datedepart = "2021-01-04",datefin = "2021-10-03")
+variants_aggregated_format <- format_variant(variants_aggregated,datedepart = "2021-01-04",datefin = "2021-10-03")
+
+# Simulator is applied on belgium cases data from 2021-01-04 to 2021-10-03
+variants_aggregated_formatted_BE <-   variants_aggregated_formatted %>% 
+  filter(country_code == "BE")
+case_aggregated_BE <-   case_aggregated %>% 
+  filter(country_code == "BE")
+resultat_desaggregated = simulator(trainset = variants_aggregated_formatted_BE,
+  testset = case_aggregated_BE,
+  geolocalisation = "country_code",
+  outcome = "variant",
+  count = 'new_cases', 
+  time= "time",
+  factor = 500)
+
+```
